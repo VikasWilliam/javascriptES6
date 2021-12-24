8d28f0bfb5ce29281b3b2b0093bbd4bfc3c84df9
VikasWilliam <vikasdenobili@outlook.com>
2021-12-24 13:49:03 +0530

    let vs var

---
+++ letVSvar.js
@@ -0,0 +1,11 @@
+let globalvariable="I am a global variable";
+
+if(true){
+
+    var localvariable="I am a local variable";
+    console.log(globalvariable);
+    console.log(localvariable);
+}
+
+console.log(globalvariable);
+    console.log(localvariable);
